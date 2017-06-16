a19e439b8c828ab4532673fb8bd7447b09d3a1d6
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-06-16 12:30:26 -0500

    Updated to save zpl string in a file

--- zebra.js
+++ zebra.js
@@ -4,15 +4,17 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser')
 
-
-
 try{
 	var fs = require('fs')	
 }
 catch(e){
 	console.log(e);
 }
-
+app.use(function (req,res,next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
 	extended : false
@@ -23,12 +25,26 @@ app.get('',function (req,res) {
 	res.send('Launched')
 })
 app.post('/setCadena',function (req,res) {
-	console.log(req);
-	var cadena = req.body.zpl;
-	console.log(cadena);
+	var cadena = req.body.params.zpl;
+	//Cambiar `diego` por el nombre de su computadora
+	fs.writeFile('C:\\Users\\diego\\Documents\\zebra.zpl', cadena , (err) => {
+		if(err){
+			res.send(JSON.stringify({ msg : err }));
+		}
+		res.send(JSON.stringify({ msg : 'Archivo Guardado exitosamente' }));
+	})
+
 })
 
+app.get('/imprimirZpl',function(req,res){
+	fs.open('C:\\Users\\diego\\Documents\\zebra.zpl','r',function (err,fd) {
+		if (err) throw err;
+		console.log(fd);
+	});
 
+	console.log('Impriendo');
+	res.send(JSON.stringify( { msg : 'Imprimiendo' } ))
+})
 /*const child = execFile('print', ['C:\\Users\\diego\\Documents\\zebra.zpl','/D:LPT2'], (error, stdout, stderr) => {
 	if (error) {
 		throw error;
@@ -39,8 +55,7 @@ app.post('/setCadena',function (req,res) {
 app.listen(3000,function() {
 	console.log('Listen Port 3000')
 });
-//console.log(fs)
-//'http://148.244.226.38/sibei_gabin/system/zebra.txt'
+
 
 //fs.open('C:\\Users\\diego\\Documents\\zebra.txt','r',function (err,fd) {
 //	console.log(err);
